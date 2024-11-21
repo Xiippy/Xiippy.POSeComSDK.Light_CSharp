8eb82029b05cf6983f13cba8cd50561bfc63d193
Xiippy <contact@xiippy.ai>
2024-11-21 20:49:44 +1100

    gitignore was added.

--- obj/Debug/netstandard2.0/Xiippy.POSeComSDK.Light.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/Xiippy.POSeComSDK.Light.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
             Xiippy.POSeComSDK.Light:1.1.0; Xiippy.ai's simplified lightweight SDK for POS and e-commerce systems. This package enables a POS system or an e-commerce system to trigger a privacy-preserving data-rich payment which includes a statement object that is delivered to the payer in a privacy-preserving fashion. This SDK interacts with Xiippy's SDKBridge which provides simplified localised interfaces and manages all the complex cryptographic and data delivery ops.
         ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+75d9ccb82348a3c051e35dc996aef1fda4953b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xiippy.POSeComSDK.Light")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xiippy.POSeComSDK.Light")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
